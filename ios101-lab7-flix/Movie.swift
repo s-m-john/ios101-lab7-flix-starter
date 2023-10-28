ca283067808953384d6a1b74ba2c7d0852faa4ae
s-m-john <smc_john@hotmail.com>
2023-10-28 15:14:07 -0400

    Step 3

--- ios101-lab7-flix/Movie.swift
+++ ios101-lab7-flix/Movie.swift
@@ -9,7 +9,7 @@ struct MovieFeed: Decodable {
     let results: [Movie]
 }
 
-struct Movie: Decodable {
+struct Movie: Codable, Equatable {
     let title: String
     let overview: String
     let posterPath: String? // Path used to create a URL to fetch the poster image
@@ -33,5 +33,49 @@ struct Movie: Decodable {
         case releaseDate = "release_date"
         case voteAverage = "vote_average"
         case id
+    }
+}
+
+
+
+// Methods for saving, retrieving, and removing movies from favorites
+extension Movie {
+    // The "Favorites" key
+    static var favoritesKey: String {
+        return "Favorites"
+    }
+
+    // Save an array of favorite movies to UserDefaults
+    static func save(_ movies: [Movie], forKey key: String) {
+        let defaults = UserDefaults.standard
+        let encodedData = try! JSONEncoder().encode(movies)
+        defaults.set(encodedData, forKey: key)
+    }
+
+    // Get the array of favorite movies from UserDefaults
+    static func getMovies(forKey key: String) -> [Movie] {
+        let defaults = UserDefaults.standard
+        if let data = defaults.data(forKey: key) {
+            let decodedMovies = try! JSONDecoder().decode([Movie].self, from: data)
+            return decodedMovies
+        } else {
+            return []
+        }
+    }
+
+    // Adds the movie to the favorites array in UserDefaults
+    func addToFavorites() {
+        var favoriteMovies = Movie.getMovies(forKey: Movie.favoritesKey)
+        favoriteMovies.append(self)
+        Movie.save(favoriteMovies, forKey: Movie.favoritesKey)
+    }
+
+    // Removes the movie from the favorites array in UserDefaults
+    func removeFromFavorites() {
+        var favoriteMovies = Movie.getMovies(forKey: Movie.favoritesKey)
+        favoriteMovies.removeAll { movie in
+            return self == movie
+        }
+        Movie.save(favoriteMovies, forKey: Movie.favoritesKey)
     }
 }
